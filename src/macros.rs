313d390a542bb81ec7fe8b765b3fb3837e51a535
wariuni <wariuni@gmail.com>
2017-06-30 10:11:11 +0900

    Refactor ::macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,15 +1,12 @@
 macro_rules! write_decorated {
-    ($attr:expr, $color: expr, $format: tt, $($x: expr), *) => (
+    ($attr:expr, $color: expr, $format: tt$(, $x: expr)*) => (
         __write_decorated!(stdout, write, $attr, $color, $format$(, $x)*)
     );
 }
 
 
 macro_rules! writeln_decorated {
-    ($attr:expr, $color: expr, $format: tt) => (
-        __write_decorated!(stdout, writeln, $attr, $color, $format)
-    );
-    ($attr:expr, $color: expr, $format: tt, $($x: expr), *) => (
+    ($attr:expr, $color: expr, $format: tt$(, $x: expr)*) => (
         __write_decorated!(stdout, writeln, $attr, $color, $format$(, $x)*)
     );
 }
@@ -23,39 +20,14 @@ macro_rules! write_error_decorated {
 
 
 macro_rules! writeln_error_decorated {
-    ($attr:expr, $color: expr, $format: tt) => (
-        __write_decorated!(stderr, writeln, $attr, $color, $format)
-    );
-    ($attr:expr, $color: expr, $format: tt, $($x: expr), *) => (
+    ($attr:expr, $color: expr, $format: tt$(, $x: expr)*) => (
         __write_decorated!(stderr, writeln, $attr, $color, $format$(, $x)*)
     );
 }
 
 
 macro_rules! __write_decorated {
-    ($out: ident, $write: ident, $attr: expr, $color: expr, $format: tt) => {
-        {
-            use std::io;
-            use term;
-
-            if let Some(mut term) = term::$out() {
-                if let Ok(_) = term.attr($attr) {
-                    if let Some(color) = $color {
-                        let _ = term.fg(color);
-                    }
-                    $write!(term, $format).unwrap();
-                    term.reset().unwrap();
-                } else {
-                    $write!(io::$out(), $format).unwrap();
-                }
-            } else {
-                $write!(io::$out(), $format).unwrap();
-            }
-            io::$out().flush().unwrap();
-        }
-    };
-
-    ($out: ident, $write: ident, $attr: expr, $color: expr, $format: tt, $($x: expr), *) => {
+    ($out: ident, $write: ident, $attr: expr, $color: expr, $format: tt$(, $x: expr)*) => {
         {
             use std::io;
             use term;
