7b48987b44cd9e38b2dd49ac3c19172d138cb460
wariuni <wariuni@gmail.com>
2017-09-08 23:06:54 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -151,22 +151,24 @@ fn main() {
         let config = config();
         let target = matches.value_of("target").unwrap();
         let cases = config.testcase_path(target).or_exit1();
-        let build_command = config.construct_build_command(target).or_exit1();
         let run_command = config.construct_run_command(target).or_exit1();
-        return judge::judge(&cases, run_command, build_command).or_exit1();
+        let build_command = config.construct_build_command(target).or_exit1();
+        return judge::judge(cases, run_command, build_command).or_exit1();
     } else if let Some(matches) = matches.subcommand_matches("submit") {
         let config = config();
         let target = matches.value_of("target").unwrap();
-        let cases = config.testcase_path(target).or_exit1();
-        let build_command = config.construct_build_command(target).or_exit1();
-        let run_command = config.construct_run_command(target).or_exit1();
         let contest = config.contest().or_exit1();
-        let lang_id = config.atcoder_lang_id(&target).or_exit1();
+        let cases = config.testcase_path(target).or_exit1();
         let src = config.src_path(&target).or_exit1();
-        judge::judge(&cases, run_command, build_command).or_exit1();
+        let run_command = config.construct_run_command(target).or_exit1();
+        let build_command = config.construct_build_command(target).or_exit1();
+        judge::judge(cases, run_command, build_command).or_exit1();
         return match config.service().or_exit1() {
             ServiceName::AtCoder => unimplemented!(),
-            ServiceName::AtCoderBeta => atcoder_beta::submit(&contest, &target, lang_id, &src),
+            ServiceName::AtCoderBeta => {
+                let lang_id = config.atcoder_lang_id(&target).or_exit1();
+                atcoder_beta::submit(&contest, &target, lang_id, &src)
+            }
         }.or_exit1();
     }
     unreachable!();
