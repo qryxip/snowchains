7f241cda857d97bd15967b4ffb4ada7e25a06f21
Ryo Yamashita <qryxip@gmail.com>
2020-07-07 22:47:14 +0900

    Remove all of the current code

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,44 @@
-use snowchains::path::AbsPathBuf;
-use snowchains::terminal::{AnsiStandardStream, TtyOrPiped};
-use snowchains::Opt;
+use std::{env, io::Write as _, process};
+use structopt::clap;
+use termcolor::{BufferedStandardStream, Color, ColorChoice, ColorSpec, WriteColor as _};
 
-use failure::{Fail, Fallible};
-use structopt::StructOpt as _;
-use termcolor::{Color, ColorSpec, WriteColor as _};
-
-use std::io::{self, BufWriter, Write as _};
-use std::process;
-
-fn main() -> Fallible<()> {
-    snowchains::signal::start_catching_ctrl_c()?;
-    let code = {
-        let opt = Opt::from_args();
-        let stdin = io::stdin();
-        let mut ctx = snowchains::Context {
-            cwd: AbsPathBuf::cwd()?,
-            login_retries: None,
-            stdin: TtyOrPiped::auto(&stdin),
-            stdout: AnsiStandardStream::new(BufWriter::new(io::stdout())),
-            stderr: AnsiStandardStream::new(BufWriter::new(io::stderr())),
-        };
-        match snowchains::run(opt, &mut ctx) {
-            Ok(code) => code,
-            Err(err) => {
-                let snowchains::Context {
-                    mut stdout,
-                    mut stderr,
-                    ..
-                } = ctx;
-                stdout.flush()?;
-                writeln!(stderr)?;
-                for (i, cause) in Fail::iter_chain(&err).enumerate() {
-                    let head = if i == 0 && err.cause().is_none() {
-                        "error: "
-                    } else if i == 0 {
-                        "    error: "
-                    } else {
-                        "caused by: "
-                    };
-                    stderr.set_color(
-                        ColorSpec::new()
-                            .set_fg(Some(Color::Ansi256(1)))
-                            .set_bold(true),
-                    )?;
-                    stderr.write_all(head.as_ref())?;
-                    stderr.reset()?;
-                    for (i, line) in cause.to_string().lines().enumerate() {
-                        if i > 0 {
-                            (0..head.len()).try_for_each(|_| stderr.write_all(b" "))?;
-                        }
-                        writeln!(stderr, "{}", line)?;
-                    }
-                }
-                let backtrace = err.backtrace().unwrap().to_string();
-                if !backtrace.is_empty() {
-                    writeln!(stderr, "\n{}", backtrace)?;
-                }
-                stderr.flush()?;
-                1
-            }
+fn main() {
+    if let Err(err) = snowchains::run(env::args_os()) {
+        if let Some(err) = err.downcast_ref::<clap::Error>() {
+            err.exit();
         }
-    };
-    process::exit(code)
+
+        // same as `clap`
+        let mut stderr = BufferedStandardStream::stderr(if atty::is(atty::Stream::Stderr) {
+            ColorChoice::Auto
+        } else {
+            ColorChoice::Never
+        });
+
+        for (i, s) in format!("{:?}", err).splitn(2, "Caused by:\n").enumerate() {
+            let _ = stderr.set_color(
+                ColorSpec::new()
+                    .set_reset(false)
+                    .set_bold(true)
+                    .set_fg(Some(Color::Red)),
+            );
+
+            if i == 0 {
+                let _ = stderr.write_all(b"Error:");
+                let _ = stderr.reset();
+                let _ = stderr.write_all(b" ");
+            } else {
+                let _ = stderr.write_all(b"Caused by:");
+                let _ = stderr.reset();
+                let _ = stderr.write_all(b"\n");
+            }
+
+            let _ = stderr.write_all(s.as_ref());
+        }
+
+        let _ = stderr.write_all(b"\n");
+        let _ = stderr.flush();
+
+        process::exit(1);
+    }
 }
