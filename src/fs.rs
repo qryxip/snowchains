7042becf25108652cd409524579ba2ca91cfdf3c
Ryo Yamashita <qryxip@gmail.com>
2020-07-22 20:38:04 +0900

    Add `--full` option to `r t`

--- src/fs.rs
+++ src/fs.rs
@@ -1,5 +1,14 @@
 use anyhow::Context as _;
+use serde::{de::DeserializeOwned, Serialize};
 use std::path::Path;
+
+pub(crate) fn read_json<T: DeserializeOwned, P: AsRef<Path>>(path: P) -> anyhow::Result<T> {
+    let path = path.as_ref();
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("Could not read `{}`", path.display()))?;
+    serde_json::from_str(&content)
+        .with_context(|| format!("Could not parse the JSON at `{}`", path.display()))
+}
 
 pub(crate) fn write(
     path: impl AsRef<Path>,
@@ -14,6 +23,14 @@ pub(crate) fn write(
 
     std::fs::write(&path, contents)
         .with_context(|| format!("Could not write `{}`", path.as_ref().display()))
+}
+
+pub(crate) fn write_json(
+    path: impl AsRef<Path>,
+    value: impl Serialize,
+    create_dir_all: bool,
+) -> anyhow::Result<()> {
+    write(path, serde_json::to_string(&value)?, create_dir_all)
 }
 
 fn create_dir_all(path: impl AsRef<Path>) -> anyhow::Result<()> {
