17f2ea8042b5c72a0dfba185715a3eedad72a569
Ryo Yamashita <qryxip@gmail.com>
2020-07-26 21:56:42 +0900

    Rename `test` to `judge`

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,7 @@
 pub(crate) mod init;
+pub(crate) mod judge;
 pub(crate) mod login;
 pub(crate) mod retrieve_languages;
 pub(crate) mod retrieve_testcases;
 pub(crate) mod submit;
-pub(crate) mod test;
 pub(crate) mod xtask;
